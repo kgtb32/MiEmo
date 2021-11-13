ad870c5f6d33d3bcbcd6a3080f2598898877de1f
Kévin GOMES TEIXEIRA <kevingomes005@gmail.com>
2021-11-13 16:20:44 +0100

    Add Router in App

--- MiEmo Webapp/src/App.jsx
+++ MiEmo Webapp/src/App.jsx
@@ -1,37 +1,21 @@
-import { React, useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { React } from 'react'
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Home from './pages/Home'
+import Settings from './pages/Settings'
+import Page404 from './pages/Page404'
 
 function App() {
-	const [count, setCount] = useState(0)
-
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>Hello Vite + React!</p>
-				<p>
-					<button type="button" onClick={() => setCount(count => count + 1)}>
-						count is: {count}
-					</button>
-				</p>
-				<p>
-					Edit <code>App.jsx</code> and save to test HMR updates.
-				</p>
-				<p>
-					<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-						Learn React
-					</a>
-					<a
-						className="App-link"
-						href="https://vitejs.dev/guide/features.html"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Vite Docs
-					</a>
-				</p>
-			</header>
+			<Router>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/settings" element={<Settings />} />
+					<Route path="/*" element={<Page404 />} />
+				</Routes>
+			</Router>
 		</div>
 	)
 }
