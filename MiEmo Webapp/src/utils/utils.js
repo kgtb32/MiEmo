eda9ee20dcbf26b54b7be13cd5a3b82da16f7813
kévin GOMES TEIXEIRA <kevingomes005@gmail.com>
2021-12-14 21:58:10 +0100

    Add generateMeteoAvailable method in utils

--- MiEmo Webapp/src/utils/utils.js
+++ MiEmo Webapp/src/utils/utils.js
@@ -2,5 +2,12 @@ export const generateQueryString = (urlStr, parameters) => {
 	const finalUrl = new URL(urlStr)
 	let params = finalUrl.searchParams
 	parameters.map(e => params.append(e.name, e.val))
-	return finalUrl.toString()
+	return decodeURIComponent(finalUrl.toString())
+}
+
+export const generateMeteoAvailable = whatWanted => {
+	return whatWanted.reduce((previousValue, currentValue) => {
+		console.log(previousValue, currentValue)
+		return previousValue + ',' + currentValue
+	})
 }
