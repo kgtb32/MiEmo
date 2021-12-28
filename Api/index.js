32bfb9979feafa0e2916554441b58a303ac89829
Kévin GOMES TEIXEIRA <kevingomes005@gmail.com>
2021-12-28 16:56:24 +0100

    Add /city/find API

--- Api/index.js
+++ Api/index.js
@@ -3,26 +3,28 @@
 import fastify from "fastify";
 import fastifyCors from "fastify-cors";
 import { get as weatherGet } from "./routes/weather.js";
+import { get as geocodingGet } from "./routes/city.js";
 
 const fastifyServer = fastify({ logger: true });
 
 fastifyServer.get("/", async (request, reply) => {
-	return { hello: "world" };
+  return { hello: "world" };
 });
 
 fastifyServer.get("/weather/get", weatherGet);
 
-fastifyServer.register( fastifyCors, {
-	origin: 'http://localhost:3000'
-  })
-  
+fastifyServer.get("/city/find", geocodingGet);
+
+fastifyServer.register(fastifyCors, {
+  origin: "http://localhost:3000",
+});
 
 const start = async () => {
-	try {
-		await fastifyServer.listen(8000);
-	} catch (err) {
-		fastifyServer.log.error(err);
-		process.exit(1);
-	}
+  try {
+    await fastifyServer.listen(8000);
+  } catch (err) {
+    fastifyServer.log.error(err);
+    process.exit(1);
+  }
 };
 start();
