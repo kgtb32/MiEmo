910de40ee22e6a9340f8d4af3be7c25f77d6331c
timothey cauchois <timotheycauchois@outlook.fr>
2022-01-12 17:21:30 +0100

    Add digitalClock widget

--- MiEmo Webapp/src/components/layout/ComponentFactory.jsx
+++ MiEmo Webapp/src/components/layout/ComponentFactory.jsx
@@ -1,9 +1,11 @@
-import Clock from '../Widgets/Clock'
+import Clock from '../Widgets/Clocks/Clock'
 import Meteo from '../Widgets/Meteo/Meteo'
+import FlipClock from '../Widgets/Clocks/FlipClock'
 
 const componentFactory = {
 	'com.miemo.widgets.meteo': Meteo,
 	'com.miemo.widgets.clock': Clock,
+	'com.miemo.widgets.flipClock': FlipClock,
 }
 
 export default componentFactory
