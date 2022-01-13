0e01da93741d70d6de6861be785b572e44e30f3f
Zola Voldi <voldi.zola@edu.itescia.fr>
2022-01-13 17:42:05 +0100

    Add youtube component

--- MiEmo Webapp/src/components/layout/ComponentFactory.jsx
+++ MiEmo Webapp/src/components/layout/ComponentFactory.jsx
@@ -1,11 +1,13 @@
 import Clock from '../Widgets/Clocks/Clock'
 import Meteo from '../Widgets/Meteo/Meteo'
 import FlipClock from '../Widgets/Clocks/FlipClock'
+import Youtube from '../Widgets/Youtube'
 
 const componentFactory = {
 	'com.miemo.widgets.meteo': Meteo,
 	'com.miemo.widgets.clock': Clock,
 	'com.miemo.widgets.flipClock': FlipClock,
+	'com.miemo.widgets.youtube': Youtube,
 }
 
 export default componentFactory
