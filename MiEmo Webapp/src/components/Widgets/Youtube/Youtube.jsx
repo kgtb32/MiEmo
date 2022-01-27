62640607b9719c71960747e0f355a74fbb4dc97e
Zola Voldi <voldi.zola@edu.itescia.fr>
2022-01-27 17:15:04 +0100

    Moved Youtube component

--- MiEmo Webapp/src/components/Widgets/Youtube/Youtube.jsx
+++ MiEmo Webapp/src/components/Widgets/Youtube/Youtube.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 const Youtube = React.lazy(() => import('react-youtube'))
 import { withSize } from 'react-sizeme'
 import PropTypes from 'prop-types'
+
 function ReactYoutube({ size }) {
 	const youtubeSize = {
 		height: size.height,
@@ -13,12 +14,10 @@ function ReactYoutube({ size }) {
 		</div>
 	)
 }
-
 ReactYoutube.propTypes = {
 	size: PropTypes.shape({
 		height: PropTypes.number,
 		width: PropTypes.number,
 	}),
 }
-
 export default withSize({ monitorHeight: true })(ReactYoutube)
--- MiEmo Webapp/src/components/layout/ComponentFactory.jsx
+++ MiEmo Webapp/src/components/layout/ComponentFactory.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 const Clock = React.lazy(() => import('../Widgets/Clocks/Clock'))
 const Meteo = React.lazy(() => import('../Widgets/Meteo/Meteo'))
 const FlipClock = React.lazy(() => import('../Widgets/Clocks/FlipClock'))
-const Youtube = React.lazy(() => import('../Widgets/Youtube'))
+const Youtube = React.lazy(() => import('../Widgets/Youtube/Youtube'))
 const Calendar = React.lazy(() => import('../Widgets/Calendars/Calendar'))
 const EmotionWidget = React.lazy(() => import('../Widgets/EmotionWidget/EmotionWidget'))
 const Activity = React.lazy(() => import('../Widgets/Activities/Activity'))
