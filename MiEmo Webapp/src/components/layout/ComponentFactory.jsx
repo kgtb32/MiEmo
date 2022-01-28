d205b21480750bd360574305eeaaea10821753cf
timothey cauchois <timotheycauchois@outlook.fr>
2022-01-28 10:44:18 +0100

    Add Calendar widget

--- MiEmo Webapp/src/components/layout/ComponentFactory.jsx
+++ MiEmo Webapp/src/components/layout/ComponentFactory.jsx
@@ -2,12 +2,14 @@ import Clock from '../Widgets/Clocks/Clock'
 import Meteo from '../Widgets/Meteo/Meteo'
 import FlipClock from '../Widgets/Clocks/FlipClock'
 import Youtube from '../Widgets/Youtube'
+import Calendar from '../Widgets/Calendars/Calendar'
 
 const componentFactory = {
 	'com.miemo.widgets.meteo': Meteo,
 	'com.miemo.widgets.clock': Clock,
 	'com.miemo.widgets.flipClock': FlipClock,
 	'com.miemo.widgets.youtube': Youtube,
+	'com.miemo.widgets.calendar': Calendar,
 }
 
 export default componentFactory
