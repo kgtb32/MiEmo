23c5cbdf56527b7e16e037d958cdc12b3f576c69
Kévin GOMES TEIXEIRA <kevingomes005@gmail.com>
2022-03-03 22:54:07 +0100

    Add Emotion detection Widget

---
+++ MiEmo Webapp/src/components/Widgets/EmotionWidget/EmotionWidget.jsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import { Button } from 'primereact/button'
+
+import { EventEmitter } from 'events'
+
+import VideoWidget from './VideoWidget'
+import WebcamSelector from './WebcamSelector'
+import ResultModal from './ResultModal'
+
+import { AiFillCamera } from 'react-icons/ai'
+import { MdCameraswitch } from 'react-icons/md'
+
+const shotEvent = new EventEmitter()
+
+function EmotionWidget() {
+	const [devices, setDevices] = useState([])
+	const [selectedVideoDevice, setSelectedVideoDevice] = useState('')
+	const [modalCameraSelectVisible, setModalCameraSelectVisible] = useState(false)
+	const [modalResultVisible, setModalResultVisible] = useState(false)
+	const [emotion, setEmotion] = useState(null)
+
+	const searchDevices = React.useCallback(
+		mediaDevices => setDevices(mediaDevices.filter(({ kind }) => kind === 'videoinput')),
+		[setDevices],
+	)
+
+	React.useEffect(() => {
+		navigator.mediaDevices.enumerateDevices().then(searchDevices)
+	}, [searchDevices])
+
+	useEffect(() => {
+		if (emotion) {
+			setModalResultVisible(true)
+		}
+	}, [emotion])
+
+	return (
+		<div className="w-100 h-100 d-flex flex-column">
+			<WebcamSelector
+				selectedVideoDevice={selectedVideoDevice}
+				videoDevices={devices}
+				setSelectedVideoDevice={setSelectedVideoDevice}
+				setModalVisible={setModalCameraSelectVisible}
+				modalVisible={modalCameraSelectVisible}
+			/>
+			<ResultModal
+				ResultModalVisible={modalResultVisible}
+				setResultModalVisible={setModalResultVisible}
+				emotion={emotion}
+			/>
+			<div className="h-100">
+				<VideoWidget shotEvent={shotEvent} setEmotion={setEmotion} />
+			</div>
+			<div className="mx-auto">
+				<JoliButton icon="pi" className="p-button-rounded mx-auto mt-2" onClick={() => shotEvent.emit('shot')}>
+					<AiFillCamera size="1.5em" />
+				</JoliButton>
+				<JoliButton
+					onClick={() => setModalCameraSelectVisible(true)}
+					icon="pi"
+					className="p-button-rounded mx-auto mt-2"
+					style={{ marginLeft: '2em !important' }}
+				>
+					<MdCameraswitch size="1.5em" />
+				</JoliButton>
+			</div>
+		</div>
+	)
+}
+
+const JoliButton = styled(Button)`
+	margin-left: 1em !important;
+	margin-right: 1em !important;
+`
+
+export default EmotionWidget
--- MiEmo Webapp/src/components/layout/ComponentFactory.jsx
+++ MiEmo Webapp/src/components/layout/ComponentFactory.jsx
@@ -3,6 +3,7 @@ import Meteo from '../Widgets/Meteo/Meteo'
 import FlipClock from '../Widgets/Clocks/FlipClock'
 import Youtube from '../Widgets/Youtube'
 import Calendar from '../Widgets/Calendars/Calendar'
+import EmotionWidget from '../Widgets/EmotionWidget/EmotionWidget'
 
 const componentFactory = {
 	'com.miemo.widgets.meteo': Meteo,
@@ -10,6 +11,7 @@ const componentFactory = {
 	'com.miemo.widgets.flipClock': FlipClock,
 	'com.miemo.widgets.youtube': Youtube,
 	'com.miemo.widgets.calendar': Calendar,
+	'com.miemo.widgets.emotionWidget': EmotionWidget,
 }
 
 export default componentFactory
