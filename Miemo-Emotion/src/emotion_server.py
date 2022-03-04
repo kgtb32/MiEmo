485826970cd08ab9b83322b02985cae8a84c79a4
Kévin GOMES TEIXEIRA <kevingomes005@gmail.com>
2022-03-04 21:14:49 +0100

    Add flask emotion_server

---
+++ Miemo-Emotion/src/emotion_server.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+from emotion_detector import emotion_detector
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+
+emotion_detector_object = emotion_detector()
+
+@app.route('/emotion/detect', methods=['POST'])
+@cross_origin()
+def hello():
+    data = request.get_json()
+    if("base64_image" not in data.keys()):
+        return {
+            "resultCode": 1000,
+            "error": "pre condition failed"
+        }
+    else:
+        return {
+            "resultCode": 0,
+            "emotion": emotion_detector_object.detect_mood(data["base64_image"])
+        }
