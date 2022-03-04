a6f5d6e5dbf410c0b507f54e3070a381e2962e56
Kévin GOMES TEIXEIRA <kevingomes005@gmail.com>
2022-03-04 21:14:12 +0100

    Add emotion_detector object

---
+++ Miemo-Emotion/src/emotion_detector.py
@@ -0,0 +1,51 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+
+class emotion_detector:
+
+    emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
+
+    def __init__(self):
+        self.model = Sequential()
+        self.model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
+        self.model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Dropout(0.25))
+
+        self.model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Dropout(0.25))
+
+        self.model.add(Flatten())
+        self.model.add(Dense(1024, activation='relu'))
+        self.model.add(Dropout(0.5))
+        self.model.add(Dense(7, activation='softmax'))
+
+        self.model.load_weights('model.h5')
+        cv2.ocl.setUseOpenCL(False)
+    
+    def detect_mood(self, base64_img):
+           # start the webcam feed
+        cap = cv2.VideoCapture(base64_img)
+            # Find haar cascade to draw bounding box around face
+        ret, frame = cap.read()
+        if not ret:
+            exit()
+        facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = facecasc.detectMultiScale(gray,scaleFactor=1.3, minNeighbors=5)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
+            roi_gray = gray[y:y + h, x:x + w]
+            cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+            prediction = self.model.predict(cropped_img)
+            maxindex = int(np.argmax(prediction))
+            return self.emotion_dict[maxindex]
+        return "not detected"
