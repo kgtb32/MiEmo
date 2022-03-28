c2d8da2727adf2f2df45b02cb8d8615a5d5bdd34
Kévin GOMES TEIXEIRA <kevingomes005@gmail.com>
2022-03-28 12:12:16 +0200

    Add links in Home page
    - Add Settings button in Home page
    - Add Store button in Home page

--- MiEmo Webapp/src/pages/Home.jsx
+++ MiEmo Webapp/src/pages/Home.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import GridLayout from '../components/layout/GridLayout'
 
+import { Button } from 'primereact/button'
+
+import { MdSettings } from 'react-icons/md'
+import { FaStore } from 'react-icons/fa'
+
 export default function Home() {
 	return (
 		<div className="bg-dark" style={{ padding: 60 }}>
+			<div className="d-flex justify-content-end">
+				<Button className="p-button-rounded p-1 px-1 mx-1">
+					<FaStore size="1.5em" />
+				</Button>
+				<Button className="p-button-rounded p-1 px-1 mx-1">
+					<MdSettings size="1.5em" />
+				</Button>
+			</div>
 			<GridLayout />
 		</div>
 	)
