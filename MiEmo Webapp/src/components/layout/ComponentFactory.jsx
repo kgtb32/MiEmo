8dd6132ccccbf350141517de75b8dc8429a4fb4f
kévin GOMES TEIXEIRA <kevingomes005@gmail.com>
2022-03-29 09:37:17 +0200

    Add drag hability in Home page

--- MiEmo Webapp/src/components/layout/ComponentFactory.jsx
+++ MiEmo Webapp/src/components/layout/ComponentFactory.jsx
@@ -7,14 +7,25 @@ const Calendar = React.lazy(() => import('../Widgets/Calendars/Calendar'))
 const EmotionWidget = React.lazy(() => import('../Widgets/EmotionWidget/EmotionWidget'))
 const Activity = React.lazy(() => import('../Widgets/Activities/Activity'))
 
+import { TiWeatherPartlySunny } from 'react-icons/ti'
+import { IoMdClock } from 'react-icons/io'
+import { IoCalendarNumberSharp } from 'react-icons/io5'
+import { AiFillClockCircle, AiFillYoutube } from 'react-icons/ai'
+import { MdEmojiEmotions } from 'react-icons/md'
+import { FiActivity } from 'react-icons/fi'
+
 const componentFactory = {
-	'com.miemo.widgets.meteo': Meteo,
-	'com.miemo.widgets.clock': Clock,
-	'com.miemo.widgets.flipClock': FlipClock,
-	'com.miemo.widgets.youtube': Youtube,
-	'com.miemo.widgets.calendar': Calendar,
-	'com.miemo.widgets.emotionWidget': EmotionWidget,
-	'com.miemo.widgets.activity': Activity,
+	'com.miemo.widgets.meteo': { item: Meteo, name: 'Météo', img: TiWeatherPartlySunny },
+	'com.miemo.widgets.clock': { item: Clock, name: 'Horloge analogique', img: IoMdClock },
+	'com.miemo.widgets.flipClock': { item: FlipClock, name: 'Horloge numérique', img: AiFillClockCircle },
+	'com.miemo.widgets.youtube': { item: Youtube, name: 'Youtube', img: AiFillYoutube },
+	'com.miemo.widgets.calendar': { item: Calendar, name: 'Calendrier', img: IoCalendarNumberSharp },
+	'com.miemo.widgets.emotionWidget': {
+		item: EmotionWidget,
+		name: "Widget détection d'émotions",
+		img: MdEmojiEmotions,
+	},
+	'com.miemo.widgets.activity': { item: Activity, name: 'Activités', img: FiActivity },
 }
 
 export default componentFactory
--- MiEmo Webapp/src/pages/Home.jsx
+++ MiEmo Webapp/src/pages/Home.jsx
@@ -1,23 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import GridLayout from '../components/layout/GridLayout'
+import { Link } from 'react-router-dom'
+
+import { EventEmitter } from 'events'
 
 import { Button } from 'primereact/button'
+
+import Store from '../components/Store/Store'
 
 import { MdSettings } from 'react-icons/md'
 import { FaStore } from 'react-icons/fa'
 
 export default function Home() {
+	const addEvent = new EventEmitter()
+	const [storeVisible, setStoreVisible] = useState(false)
+
 	return (
 		<div className="bg-dark" style={{ padding: 60 }}>
+			{storeVisible && <Store addEvent={addEvent} />}
 			<div className="d-flex justify-content-end">
 				<Button className="p-button-rounded p-1 px-1 mx-1">
+					<Link to="/settings">
+						<MdSettings size="1.5em" color="black" />
+					</Link>
+				</Button>
+				<Button className="p-button-rounded p-1 px-1 mx-1" onClick={() => setStoreVisible(!storeVisible)}>
 					<FaStore size="1.5em" />
 				</Button>
-				<Button className="p-button-rounded p-1 px-1 mx-1">
-					<MdSettings size="1.5em" />
-				</Button>
 			</div>
-			<GridLayout />
+			<GridLayout addEvent={addEvent} />
 		</div>
 	)
 }
