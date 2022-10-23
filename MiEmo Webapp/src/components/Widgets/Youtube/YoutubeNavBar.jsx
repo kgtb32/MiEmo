5663da4d6f47c6adf32f1df39f1ab344c935bbe1
Jajoute <invictusrokenson@gmail.com>
2022-10-23 23:13:14 +0200

    add YoutubeNavBar hover effects

--- MiEmo Webapp/src/components/Widgets/Youtube/YoutubeNavBar.jsx
+++ MiEmo Webapp/src/components/Widgets/Youtube/YoutubeNavBar.jsx
@@ -1,59 +1,84 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { VscHistory } from 'react-icons/vsc'
 import { MdOutlineFavorite } from 'react-icons/md'
-import { IoSearchCircleOutline } from 'react-icons/io5'
+import { HiOutlineSearch } from 'react-icons/hi'
+import styled from 'styled-components'
 
 const YoutubeNavBar = () => {
+	const [navItem, setNavItem] = useState({ history: false, search: true, favorites: false })
+	const [isHover, setIsHover] = useState({ history: false, search: true, favorites: false })
+	const changeValue = (history, search, favorites) => {
+		return { history: history, search: search, favorites: favorites }
+	}
+
+	const MouseOutStyle = value => {
+		return {
+			color: navItem[value] ? 'red' : 'black',
+			borderBottom: navItem[value] ? '4px solid' : 'none',
+			borderTop: navItem[value] ? '4px solid white' : 'none',
+		}
+	}
+
+	const ColorSearch = isHover.search && !navItem.search ? MouseHoverStyle : MouseOutStyle('search')
+	const History = isHover.history && !navItem.history ? MouseHoverStyle : MouseOutStyle('history')
+	const Favorites = isHover.favorites && !navItem.favorites ? MouseHoverStyle : MouseOutStyle('favorites')
+
 	return (
-		<div
-			style={{
-				height: '20%',
-				backgroundColor: 'white',
-				display: 'flex',
-				justifyContent: 'space-around',
-				alignItems: 'center',
-				flexDirection: 'row',
-				flexWrap: 'wrap',
-			}}
-		>
-			<div
-				style={{
-					display: 'flex',
-					flexDirection: 'column',
-					color: 'black',
-					alignItems: 'center',
-					flexGrow: '1',
-				}}
+		<DivContainer>
+			<DivNavItem
+				style={History}
+				onClick={() => setNavItem(changeValue(true, false, false))}
+				onMouseEnter={() => setIsHover(changeValue(true, false, false))}
+				onMouseLeave={() => setIsHover(changeValue(false, false, false))}
 			>
 				<VscHistory />
 				<p style={{ margin: '0' }}>Historique</p>
-			</div>
-			<div
-				style={{
-					display: 'flex',
-					flexDirection: 'column',
-					color: 'black',
-					alignItems: 'center',
-					flexGrow: '1',
-				}}
+			</DivNavItem>
+			<DivNavItem
+				style={ColorSearch}
+				onClick={() => setNavItem(changeValue(false, true, false))}
+				onMouseEnter={() => setIsHover(changeValue(false, true, false))}
+				onMouseLeave={() => setIsHover(changeValue(false, false, false))}
 			>
-				<IoSearchCircleOutline />
+				<HiOutlineSearch size={16} />
 				<p style={{ margin: '0' }}>Rechercher</p>
-			</div>
-			<div
-				style={{
-					display: 'flex',
-					flexDirection: 'column',
-					color: 'black',
-					alignItems: 'center',
-					flexGrow: '1',
-				}}
+			</DivNavItem>
+			<DivNavItem
+				style={Favorites}
+				onClick={() => setNavItem(changeValue(false, false, true))}
+				onMouseEnter={() => setIsHover(changeValue(false, false, true))}
+				onMouseLeave={() => setIsHover(changeValue(false, false, false))}
 			>
 				<MdOutlineFavorite />
 				<p style={{ margin: '0' }}>Favoris</p>
-			</div>
-		</div>
+			</DivNavItem>
+		</DivContainer>
 	)
+}
+
+const DivContainer = styled.div`
+	height: 20%;
+	background-color: white;
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+	flex-direction: row;
+	flex-wrap: wrap;
+`
+const DivNavItem = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	flex-grow: 1;
+	align-self: stretch;
+	justify-content: center;
+`
+export const MouseHoverStyle = {
+	color: 'white',
+	borderBottom: '4px solid #ff000091',
+	backgroundColor: '#ff9c9c',
+	fontWeight: 'bolder',
+	borderTop: '4px solid #ff9c9c',
 }
 
 export default YoutubeNavBar
