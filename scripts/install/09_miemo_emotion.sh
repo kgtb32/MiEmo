742ee9daf2c0df3adf58fa22b1d9b2fbc18d3790
Kévin GOMES TEIXEIRA <kevingomes005@gmail.com>
2022-11-05 23:04:29 +0100

    Add pull command in miemo-emotion install

--- scripts/install/09_miemo_emotion.sh
+++ scripts/install/09_miemo_emotion.sh
@@ -5,5 +5,8 @@ dialog --title "INSTALLING MIEMO" --backtitle "miemo (ARCADE MODE) V2.0" \
 
 cp /tmp/miemo/config/docker-compose.yml /home/miemo/
 
+dialog --backtitle "miemo (ARCADE MODE) V2.0" --infobox "PULLING MIEMO-EMOTION." 3 60 &
+docker pull kgtb32/miemo-emotion &>> /tmp/miemo_install.log
+
 clear
 dialog --backtitle "miemo (ARCADE MODE) V2.0" --infobox "MIEMO EMOTION INSTALLED." 3 60 &
