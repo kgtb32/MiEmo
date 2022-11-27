5e25a4e2ff85b6e6e4dabf7a2aa20a42e32ab6d0
Jajoute <invictusrokenson@gmail.com>
2022-11-27 23:45:16 +0100

    recognition ready to use

--- MiEmo Webapp/src/components/Recognition/constant.jsx
+++ MiEmo Webapp/src/components/Recognition/constant.jsx
@@ -6,10 +6,38 @@ export const allComponentKey = Object.keys(componentFactory)
 export const command = (value, widgetEventManager, setIsClick, showInfo) => {
 	value = normalize(value)
 	switch (true) {
-		case isMatch(detect[0].youtube, value):
-			widgetEventManager.emit('itemAdd', allComponentKey[3])
-			setIsClick(false)
-			showInfo('Votre Wideget ' + allComponent[3].name + ' est ajouté')
+		case isMatch(detect[0].meteo, value):
+			action(0, widgetEventManager, setIsClick, showInfo)
+			break
+		case isMatch(detect[1].clock, value):
+			action(1, widgetEventManager, setIsClick, showInfo)
+			break
+		case isMatch(detect[2].flipClock, value):
+			action(2, widgetEventManager, setIsClick, showInfo)
+			break
+		case isMatch(detect[3].youtube, value):
+			action(3, widgetEventManager, setIsClick, showInfo)
+			break
+		case isMatch(detect[4].calendar, value):
+			action(4, widgetEventManager, setIsClick, showInfo)
+			break
+		case isMatch(detect[5].emotion, value):
+			action(5, widgetEventManager, setIsClick, showInfo)
+			break
+		case isMatch(detect[6].activity, value):
+			action(6, widgetEventManager, setIsClick, showInfo)
+			break
+		case isMatch(detect[7].radio, value):
+			action(7, widgetEventManager, setIsClick, showInfo)
+			break
+		case isMatch(detect[8].todo, value):
+			action(8, widgetEventManager, setIsClick, showInfo)
+			break
+		case isMatch(detect[9].drawing, value):
+			action(9, widgetEventManager, setIsClick, showInfo)
+			break
+		case isMatch(detect[10].whitenoise, value):
+			action(10, widgetEventManager, setIsClick, showInfo)
 			break
 		default:
 			console.log(detect[0].youtube)
@@ -30,8 +58,45 @@ export const normalize = value => {
 		.toLowerCase()
 }
 
+export const action = (index, widgetEventManager, setIsClick, showInfo) => {
+	widgetEventManager.emit('itemAdd', allComponentKey[index])
+	setIsClick(false)
+	showInfo('Votre Wideget ' + allComponent[index].name + ' est ajouté')
+}
+
+//should be Lowercase without special character
 export const detect = [
 	{
-		youtube: ['youtube', 'video'],
+		meteo: ['meteo'],
+	},
+	{
+		clock: ['horloge analogique'],
+	},
+	{
+		flipClock: ['horloge numerique'],
+	},
+	{
+		youtube: ['youtube'],
+	},
+	{
+		calendar: ['calendrier'],
+	},
+	{
+		emotion: ['emotion'],
+	},
+	{
+		activity: ['activite'],
+	},
+	{
+		radio: ['radio'],
+	},
+	{
+		todo: ['tache'],
+	},
+	{
+		drawing: ['dessiner', 'dessine'],
+	},
+	{
+		whitenoise: ['sons', 'blancs', 'son'],
 	},
 ]
