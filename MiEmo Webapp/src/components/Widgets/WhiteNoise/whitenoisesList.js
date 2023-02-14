66f8a332d50c2672521ff4edaa3d882623c829ef
Kévin GOMES TEIXEIRA <kevingomes005@gmail.com>
2023-02-14 16:12:39 +0100

    Add Seemlessed white noises

--- MiEmo Webapp/src/components/Widgets/WhiteNoise/whitenoisesList.js
+++ MiEmo Webapp/src/components/Widgets/WhiteNoise/whitenoisesList.js
@@ -1,2 +1,72 @@
+import LakeWhiteNoise from '/sounds/whitenoise/lake.ogg'
+import LakeIcon from '/img/whitenoise/lake.png'
+
+import SnowIcon from '@bybas/weather-icons/production/fill/all/snow.svg'
+import SnowStormWhiteNoise from '/sounds/whitenoise/Snowstorm.ogg'
+
+import SnowStormCabinWhiteNoise from '/sounds/whitenoise/cabinSnowStorm.ogg'
+import SnowStormCabinIcon from '/img/whitenoise/snowstormcabin.png'
+
+import ThunderStormWhiteNoise from '/sounds/whitenoise/thunderstorm.ogg'
+import ThunderStormIcon from '@bybas/weather-icons/production/fill/all/thunderstorms-rain.svg'
+
+import HouseRainWhiteNoise from '/sounds/whitenoise/houseRain.ogg'
+import HouseRainIcon from '/img/whitenoise/houserain.png'
+
+import NightWhiteNoise from '/sounds/whitenoise/night.ogg'
+import NightIcon from '/img/whitenoise/night.svg'
+
+import OceanWhiteNoise from '/sounds/whitenoise/ocean.ogg'
+import OceanIcon from '/img/whitenoise/ocean.svg'
+
+import ForestWhiteNoise from '/sounds/whitenoise/forest.ogg'
+import ForestIcon from '/img/whitenoise/forest.svg'
+
+import FireWhiteNoise from '/sounds/whitenoise/fire.ogg'
+import FireIcon from '/img/whitenoise/fire.png'
+
+import TrainWhiteNoise from '/sounds/whitenoise/train.ogg'
+import TrainIcon from '/img/whitenoise/train.svg'
+
 export default [
+	{
+		src: LakeWhiteNoise,
+		icon: LakeIcon,
+	},
+	{
+		src: SnowStormWhiteNoise,
+		icon: SnowIcon,
+	},
+	{
+		src: SnowStormCabinWhiteNoise,
+		icon: SnowStormCabinIcon,
+	},
+	{
+		src: ThunderStormWhiteNoise,
+		icon: ThunderStormIcon,
+	},
+	{
+		src: HouseRainWhiteNoise,
+		icon: HouseRainIcon,
+	},
+	{
+		src: NightWhiteNoise,
+		icon: NightIcon,
+	},
+	{
+		src: OceanWhiteNoise,
+		icon: OceanIcon,
+	},
+	{
+		src: ForestWhiteNoise,
+		icon: ForestIcon,
+	},
+	{
+		src: FireWhiteNoise,
+		icon: FireIcon,
+	},
+	{
+		src: TrainWhiteNoise,
+		icon: TrainIcon,
+	},
 ]
